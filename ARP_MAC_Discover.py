4cde8bf01c5b22100b5be06b2d0e40febc1b2b87
jonathankaula <jonaxkaul@gmail.com4>
2024-02-09 00:51:12 -0800

    update

--- ARP_MAC_Discover.py
+++ ARP_MAC_Discover.py
@@ -1,10 +1,11 @@
 from scapy.all import ARP, Ether, srp
 
-def arp_scan(ip):
+def arp_mac_scan(ip):
     arp_request = ARP(pdst=ip)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Broadcast MAC address
 
     arp_broadcast = ether/arp_request
+    print(f"ARP BROADCAST PACKET: {arp_request}")
     answered_list = srp(arp_broadcast, timeout=1, verbose=False)[0]
 
     devices = []
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     target_ip = "192.168.88.246/24"  # Adjust this to your network range
 
     print("Scanning...")
-    devices_found = arp_scan(target_ip)
+    devices_found = arp_mac_scan(target_ip)
     print("Devices found:")
     for device in devices_found:
         print(f"IP: {device['ip']}\tMAC: {device['mac']}")
