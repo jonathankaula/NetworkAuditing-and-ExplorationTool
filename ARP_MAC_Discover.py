c6506ab34c13078c49bc8a523aa2932226156dd4
jonathankaula <jonaxkaul@gmail.com4>
2024-02-08 23:45:27 -0800

    Discover CLients on a LAN using ARP and scapy

---
+++ ARP_MAC_Discover.py
@@ -0,0 +1,23 @@
+from scapy.all import ARP, Ether, srp
+
+def arp_scan(ip):
+    arp_request = ARP(pdst=ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Broadcast MAC address
+
+    arp_broadcast = ether/arp_request
+    answered_list = srp(arp_broadcast, timeout=1, verbose=False)[0]
+
+    devices = []
+    for element in answered_list:
+        device = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        devices.append(device)
+    return devices
+
+if __name__ == "__main__":
+    target_ip = "192.168.88.246/24"  # Adjust this to your network range
+
+    print("Scanning...")
+    devices_found = arp_scan(target_ip)
+    print("Devices found:")
+    for device in devices_found:
+        print(f"IP: {device['ip']}\tMAC: {device['mac']}")
